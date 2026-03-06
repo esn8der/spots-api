57fdc9d45f371c0ced55b6cda6dc0b25c7ae3738
esn8der <ballesta.esneider@gmail.com>
2026-03-05 22:04:45 -0500

    Initialize project with Spring Boot setup and Maven Wrapper configuration.

---
+++ src/test/java/com/sioma/spotsapi/SpotsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sioma.spotsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
